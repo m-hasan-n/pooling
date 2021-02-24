d913aa77177288aac6141e9c54ccf4c4ac0b7e7e
m-hasan-n <eng.mhasan@gmail.com>
2021-02-24 13:35:41 +0000

    code

--- evaluate.py
+++ evaluate.py
@@ -6,33 +6,39 @@ from torch.utils.data import DataLoader
 import time
 import numpy as np
 
-# Network Arguments
-#-------------------
-args = {}
-args['use_cuda'] = True
-args['encoder_size'] = 64
-args['decoder_size'] = 128
-args['in_length'] = 16
-args['out_length'] = 25
-args['grid_size'] = (13,3)
-args['dyn_embedding_size'] = 32
-args['input_embedding_size'] = 32
-args['num_lat_classes'] = 3
-args['num_lon_classes'] = 3
+#Import the argumnets
+from exp_args import args
+
+# # Network Arguments
+# #-------------------
+# args = {}
+# args['use_cuda'] = True
+# args['encoder_size'] = 64
+# args['decoder_size'] = 128
+# args['in_length'] = 16
+# args['out_length'] = 25
+# args['grid_size'] = (13,3)
+# args['dyn_embedding_size'] = 32
+# args['input_embedding_size'] = 32
+# args['num_lat_classes'] = 3
+# args['num_lon_classes'] = 3
+# args['train_flag'] = False
+#
+# # Dimensionality of the input:
+# # 2D (X and Y or R and Theta)
+# # 3D (adding velocity as a 3d dimension)
+# args['input_dim'] = 3
+#
+# # Using Intention module?
+# args['intention_module'] = True
+#
+# # Choose the pooling mechanism
+# # 'slstm', 'cslstm', 'sgan', 'polar'
+# # -----------------------------
+# args['pooling'] = 'polar'
+
+# Evaluation mode
 args['train_flag'] = False
-
-# Dimensionality of the input:
-# 2D (X and Y or R and Theta)
-# 3D (adding velocity as a 3d dimension)
-args['input_dim'] = 3
-
-# Using Intention module?
-args['intention_module'] = True
-
-# Choose the pooling mechanism
-# 'slstm', 'cslstm', 'sgan', 'polar'
-# -----------------------------
-args['pooling'] = 'polar'
 
 if args['pooling'] == 'slstm':
     args['kernel_size'] = (4, 3)
