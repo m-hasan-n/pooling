df365d1836c30707163f471dfd10bebd4f431371
m-hasan-n <eng.mhasan@gmail.com>
2021-04-29 02:45:19 +0100

    Model Training

--- train.py
+++ train.py
@@ -42,8 +42,7 @@ crossEnt = torch.nn.BCELoss()
 
 ## Initialize data loaders
 valSet = ngsimDataset('data/ValSet.mat')
-trSet = ngsimDataset('data/ValSet.mat')
-# trSet = ngsimDataset('data/TrainSet.mat')
+trSet = ngsimDataset('data/TrainSet.mat')
 
 
 trDataloader = DataLoader(trSet,batch_size=batch_size,shuffle=True,num_workers=8,collate_fn=trSet.collate_fn)
@@ -218,7 +217,7 @@ model_fname = 'trained_models/'+args['pooling']
 if args['intention_module']:
 
     if args['input_dim']==3:
-        model_fname = model_fname + 'vel_int.tar'
+        model_fname = model_fname + '_vel_int.tar'
     else:
         model_fname = model_fname + '_int.tar'
 
